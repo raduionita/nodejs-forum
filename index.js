75054359d23227e668d15188f4f45128edb40fcf
unknown <radu.ionita@G1-AS00343.activesoft.ro>
2014-03-28 18:33:17 +0200

    Postgres

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var logfmt  = require('logfmt');
+var pg      = require('pg');
 
 var app = express();
+
+var DATABAS_URL = 'postgres://kordnmeitzjojc:8LiraiRea-VUnjIWkSWoMYno-3@ec2-54-197-237-171.compute-1.amazonaws.com:5432/detcve4tb7vbg8';
 
 app.configure(function() {
   app.set('views', __dirname +'/views');
@@ -18,6 +21,18 @@ app.get('/', function(req, res) {
   res.render('index', { 
     pageTitle: 'Basic NodeJS Forum'
   });
+});
+
+app.get('/users', function(req, res) {
+  pg.connect(DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM users', function(err, result) {
+      done();
+      if(err)
+        return console.error(err);
+      console.log(result.rows);
+    });
+  });
+  res.send('users');
 });
 
 var port = Number(process.env.PORT || 5000);
