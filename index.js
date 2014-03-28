705fa82ba7cabba9f06a3d15830585847369783b
unknown <radu.ionita@G1-AS00343.activesoft.ro>
2014-03-28 12:11:04 +0200

    'jade'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var logfmt  = require('logfmt');
 
 var app = express();
 
-app.configuration(function() {
+app.configure(function() {
   app.set('views', __dirname +'/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
