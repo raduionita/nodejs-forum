d5e1b6d0a415f62b9fa282dcb14fb98457304ae9
utharus <ionita.radu.ionut@gmail.com>
2014-03-30 14:31:06 +0300

    new file:   routes/forum.js 	new file:   routes/index.js 	new file:   routes/topic.js

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+module.export = function() {
+  return {
+    index: function(req, res) {
+      res.render('index', { 
+        pageTitle: 'Node Forum'
+      });
+    }
+    error404: function(req, res) {
+      res.send('404 Not Found!');
+    }
+  };
+};
